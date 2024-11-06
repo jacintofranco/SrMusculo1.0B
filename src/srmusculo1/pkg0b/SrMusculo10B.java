ccfe5b36d0d42c3946c2d609562ddf0cb45fdc61
jacintofranco <jacinto.franco@ifmt.edu.br>
2024-11-06 09:57:53 -0300

    first

---
+++ src/srmusculo1/pkg0b/SrMusculo10B.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package srmusculo1.pkg0b;
+
+/**
+ *
+ * @author geekaia
+ */
+public class SrMusculo10B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        LoginWin logWin = new LoginWin();  
+        logWin.setVisible(true);
+    }
+    
+}
